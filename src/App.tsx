04c0c38c1e7be33bf6d7550d78d9e828b75b3eaf
landonmulraine <landonmulraine@gmail.com>
2026-05-08 08:23:46 -0500

    fixed a squiggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "./utils/supabase";
 import type { Tweet } from "./types/Tweet";
 import {
